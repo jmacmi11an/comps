509b84150414b16600afb1297ac9e85865d179c6
Jonathan C MacMillan <jmacmill@gmail.com>
2023-03-18 15:48:06 +1000

    Add skeleton Button component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import Button from "./Button";
+
 function App() {
-    return <div>app</div>
+    return <div>
+        app
+        <div>
+            <Button>Plain</Button>
+        </div>
+       <div>
+            <Button>Primary</Button>
+        </div>
+        <div>
+            <Button>Secondary</Button>
+        </div>        
+        <div>
+            <Button>Success</Button>
+        </div>        
+        <div>
+            <Button>Warning</Button>
+        </div>        
+        <div>
+            <Button>Danger</Button>
+        </div>        
+
+    </div>
 }
 
 export default App;
