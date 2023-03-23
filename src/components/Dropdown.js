4bcbbde3d2f8de77ae0037197b8d714533e09d30
Jonathan C MacMillan <jmacmill@gmail.com>
2023-03-23 13:23:41 +1000

    Create Panel component

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { GoChevronDown } from "react-icons/go"
+import Panel from "./Panel";
 
 function Dropdown({ options, onChange, value }) {
     const [isOpen, setIsOpen] = useState(false);
@@ -11,12 +13,15 @@ function Dropdown({ options, onChange, value }) {
     }
 
     const renderedOptions = options.map((option) => {
-        return <div onClick={() => handleOptionClick(option)} key={option.value}>{option.label}</div>
+        return <div className="hover:bg-sky-100 rounded cursor-pointer p-1" onClick={() => handleOptionClick(option)} key={option.value}>{option.label}</div>
     });
 
-    return <div>
-            <div onClick={handleClick}>{value?.label || 'Select...'}</div>
-            {isOpen && <div>{renderedOptions}</div>}
+    return <div className="w-48 relative">
+            <Panel className="flex justify-between items-center cursor-pointer" onClick={handleClick}>
+                {value?.label || 'Select...'}
+                <GoChevronDown className="text-lg"/>
+            </Panel>
+            {isOpen && <Panel className="absolute top-full" >{renderedOptions}</Panel>}
         </div>
 }
 
