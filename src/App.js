2d3316154c75c9df32c72db95a28deee4c0039be
Jonathan C MacMillan <jmacmill@gmail.com>
2023-03-25 15:37:19 +1000

    Create NavigationContext.Provider skeleton

--- src/App.js
+++ src/App.js
@@ -1,19 +1,5 @@
-import Dropdown from "./components/Dropdown";
-import { useState } from "react";
-
-function App() {
-    const [selected, setSelected] = useState(null);
-
-    const handleSelect = (option) => setSelected(option);
-
-    const options = [
-        {label: 'Red', value: 'red'},
-        {label: 'Blue', value: 'blue'},
-        {label: 'Green', value: 'green'}
-    ]
-
-    return <div className="flex">
-        <Dropdown value={selected} onChange={handleSelect} options={options}></Dropdown>
+function App() {    
+    return <div>
     </div>
 }
 
