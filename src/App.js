41e5ce2ba9c3eca2622bad50a7fcafa5dbd03204
Jonathan C MacMillan <jmacmill@gmail.com>
2023-03-26 11:15:27 +1000

    Build Link component and test in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import Link from "./components/Link";
+
 function App() {    
     return <div>
+        <Link to="/accordion">Go to accordion</Link>
+        <Link to="/dropdown">Go to dropdown</Link>
     </div>
 }
 
