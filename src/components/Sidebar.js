3182105086b75208d0942559cf72162d21ad9339
Jonathan C MacMillan <jmacmill@gmail.com>
2023-03-27 23:18:22 +1000

    New commit for originally planned changes

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -19,7 +19,7 @@ function Sidebar() {
     })
 
     return (
-        <div className="sticky top-0 overflow-y-scroll flex flex-col">
+        <div className="sticky top-0 overflow-y-scroll flex flex-col items-start">
             {renderedLinks}
         </div>
     )
