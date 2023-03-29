2ad7bb507568878e0da273054f5326ab61d8275a
Jonathan C MacMillan <jmacmill@gmail.com>
2023-03-29 16:18:17 +1000

    Refactor Table to be reusable and add TablePage config

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,8 +1,31 @@
-function Table() {
+function Table({ data, config, keyFn }) {
+    const renderedHeaders = config.map((column) => {
+        return <th key={column.label}>{column.label}</th>
+    })
+
+    const renderedRows = data.map((item) => {
+        const renderedCells = config.map((column) => {
+            return <td className="p-2" key={column.label}>{column.render(item)}</td>
+        })
+
+        return (
+        <tr className="border-b" key={keyFn(item)}>
+            {renderedCells}
+        </tr>
+        );
+    });
+
     return (
-        <div>
-            Tihjs is a table!
-        </div>
+        <table className="table-auto border-spacing-2">
+            <thead>
+                <tr className="border-b-2">
+                    {renderedHeaders}
+                </tr>
+            </thead>
+            <tbody>
+                {renderedRows}
+            </tbody>
+        </table>
     )
 }
 
